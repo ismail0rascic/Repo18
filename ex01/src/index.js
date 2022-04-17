5af8a170b7c1c66d8b4acb48e2ebf4cfbd5904ed
“ismail0rascic” <ismail0rascic.gmail.com>
2022-04-18 00:13:56 +0200

    index

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -7,11 +7,11 @@ function* multiplication(x) {
         yield x;
     }
 }
-var num = multiplication(3);
+var num
+num = multiplication(3);
 
 for (let i = 0; i < 3; i++) {
     console.log(num.next().value);
-
 }
 
 module.exports = multiplication;
